7dae193a51394c1ee3e18eb37d8eafb8075c56f6
2510699771@qq.com <251069971@qq.com>
2022-12-04 02:00:12 +0800

    glft 模型添加

--- src/demo/demo3.js
+++ src/demo/demo3.js
@@ -1,105 +1,20 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { MTLLoader, OBJLoader } from "three-obj-mtl-loader";   //引入加载外部模型
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GridHelper, AxesHelper } from 'three'   //用于 instanceof 过滤
 // 1.创建场景
 const scene = new THREE.Scene()
 //  2.创建相机(透视相机)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-const axesHelper = new THREE.AxesHelper(5);
+// const axesHelper = new THREE.AxesHelper(5);
+// axesHelper.name = 'axesHelper'
+const animationMixer = new THREE.AnimationMixer(scene)
 
+const gltfLoader = new GLTFLoader()
 
-// var material = new THREE.MeshLambertMaterial({
-//     color: 0x0000ff,
-// }); //材质对象Material
-// console.log(material);
-// console.log(material.toJSON());
-// console.log(JSON.stringify(material));
-
+const clock = new THREE.Clock()
 
 let event = {}
-event.onLoad = function (obj) {
-    console.log('加载完成', obj);
-    scene.add(obj);
-}
-
-
-event.onProgress = function (url, itemsLoaded, itemsTotal) {
-    console.log('加载过程中' + url);
-}
-
-
-event.onError = function (url) {
-    console.log('加载错误' + url);
-}
-
-
-
-
-//纹理加载器   (被manager 统一管理)
-const textureLoader = new THREE.TextureLoader()
-
-
-
-
-// 纹理加载
-const preview = textureLoader.load('./textures/wfu/wfufefu_Popup_3840_sp.jpg')
-const wfuAlbedo = textureLoader.load('./textures/wfu/wfufefu_2K_Albedo.jpg')
-const wfuAo = textureLoader.load('./textures/wfu/wfufefu_2K_AO.jpg')
-const wfuDisplacementMap = textureLoader.load('./textures/wfu/wfufefu_2K_Displacement.jpg')
-const wfuNormal = textureLoader.load('./textures/wfu/wfufefu_2K_Normal.jpg')
-const wfuRoughnessMap = textureLoader.load('./textures/wfu/wfufefu_2K_Roughness.jpg')
-const wfuBump = textureLoader.load('./textures/wfu/wfufefu_2K_Bump.jpg')
-
-
-
-
-// loader.setPath(path);
-// loader.setMaterials(material)
-// loader.load('./textures/objData/untitled1.obj', event.onLoad, event.onProgress, event.onError)
-
-
-let path = "../public/static/untitled1";
-
-
-// let path = "./statics/models/obj/IronMan/";
-let objName = "untitled1.obj";
-let mtlName = "untitled1.mtl";
-
-function initModel() {
-    try {
-        // 1 初始化纹理加载器
-        var mtlLoader = new MTLLoader();
-        // 2 设置mtl资源路径
-        mtlLoader.resourcePath = path;
-        mtlLoader.path = path;
-        // 3 加载材质
-        mtlLoader.load(mtlName, function (materials) {
-            materials.preload();
-            // 4 加载模型
-            var objLoader = new THREE.OBJLoader();
-
-            objLoader.setMaterials(materials);
-            objLoader.setPath(path);
-            objLoader.load(objName, function (obj) {
-                // obj.position.y = 10;
-                // obj.scale.set(2, 2, 2);
-                // scene.add(obj);
-            })
-        })
-    } catch (e) {
-        console.error('解析失败', e);
-    }
-}
-
-initModel();
-
-
-
-camera.position.set(0, 0, 30)
-scene.add(axesHelper);
-scene.add(camera)
-
 
 
 // 初始化渲染器
@@ -109,6 +24,104 @@ render.setSize(window.innerWidth, window.innerHeight)
 
 // 将webgl渲染的canvas内容添加到body上
 document.body.appendChild(render.domElement)
+
+let action = null
+
+event.onLoad = function (obj) {
+    console.log('加载完成', obj)
+
+    obj.scene.name = 'Soldier'
+    scene.add(obj.scene);
+    scene.add(new THREE.AmbientLight(0xffffff, 1));  //环境光
+    const animationClip = obj.animations.find((animationClip) => { return animationClip.name === 'Walk' })
+    console.log(animationClip, 'animationClip');
+    action = animationMixer.clipAction(animationClip)
+    action.play()
+
+}
+
+// 递归判断
+function isClickSoldier(object) {
+    if (object.name === 'Soldier') {
+        return object
+    } else if (object.parent) {
+        return isClickSoldier(object.parent)
+    } else {
+        return null
+    }
+}
+
+
+// 光线投射
+const raycaster = new THREE.Raycaster();
+
+// 点击停止动画
+render.domElement.addEventListener('click', event => {
+    let { clientX, clientY } = event
+    let x = (clientX / window.innerWidth) * 2 - 1
+    let y = - (clientY / window.innerHeight) * 2 + 1
+    const pointer = new THREE.Vector2(x, y);
+
+    raycaster.setFromCamera(pointer, camera)
+
+    const intersects = raycaster.intersectObjects(scene.children, true)
+
+    const intersect = intersects.filter(inter => {
+        return inter.object.name !== 'axesHelper' && inter.object.name !== 'gridHelper' && inter.object.name !== 'plane'
+    })[0]  //过滤掉辅助网格 和 白色地板
+
+    // console.log(intersect, 'intersect');
+    if (intersect && isClickSoldier(intersect.object)) {
+        console.log(1, 'action');
+        action.stop()
+    }
+
+})
+
+
+
+
+
+event.onProgress = function () {
+    console.log('加载过程中');
+}
+
+
+event.onError = function (url) {
+    console.log('加载错误' + url);
+}
+
+const gltfPath = "./public/static/Soldier.glb";
+
+gltfLoader.load(gltfPath, event.onLoad, event.onProgress, event.onError)
+
+
+// 平面 -------------
+
+const geometry = new THREE.PlaneGeometry(50, 50);
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(geometry, material);
+plane.name = 'plane'
+plane.rotation.x = Math.PI / 2
+
+scene.add(plane);
+
+
+// 网格辅助---------------
+
+const gridHelper = new THREE.GridHelper(50, 50);
+gridHelper.name = 'gridHelper'
+scene.add(gridHelper);
+
+
+
+camera.position.set(0, 20, 20)
+// scene.add(axesHelper);
+scene.add(camera)
+
+
+
+
 
 
 // 使用渲染器 通过相机 将场景渲染进来
@@ -131,6 +144,7 @@ controls.enableDamping = true
 function rendeer(time) {
     controls.update()  //更新阻尼
     render.render(scene, camera);
+    animationMixer.update(clock.getDelta())
     //  渲染下一帧的时候就会调用rendeer函数
     requestAnimationFrame(rendeer)
 }
@@ -139,15 +153,15 @@ rendeer()
 
 
 // 全屏 or  退出全屏
-window.addEventListener('dblclick', () => {
-    //    双击控制屏幕进入全屏 退出全屏
-    const fullScreenElement = document.fullscreenElement
-    if (!fullScreenElement) {
-        render.domElement.requestFullscreen()
-    } else {
-        document.exitFullscreen()
-    }
-})
+// window.addEventListener('dblclick', () => {
+//     //    双击控制屏幕进入全屏 退出全屏
+//     const fullScreenElement = document.fullscreenElement
+//     if (!fullScreenElement) {
+//         render.domElement.requestFullscreen()
+//     } else {
+//         document.exitFullscreen()
+//     }
+// })
 
 
 
